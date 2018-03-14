8925cc13bedcb9f2600d92bbafa68c7922b0843f
Daiki-Konno <dai.kon1006@gmail.com>
2018-03-14 10:26:37 +0000

    week3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/blogs' => 'blogs#index'
+  get '/blogs/new' => 'blogs#new'
+  post '/blogs' => 'blogs#create'
+  get '/blogs' => 'blogs#index'
+  get '/blogs/:id' => 'blogs#show'
 end
